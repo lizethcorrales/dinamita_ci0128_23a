25084e311f1babf889cf0256ec4c584e6f0667dc
Sabrina Brenes Hernández <saindell.brenes@ucr.ac.cr>
2023-04-24 21:06:37 -0600

    Se cambia el formato de los archivos del formulario para la cantidad de personas de html a cshtml, se separa su .css y su .js en carpetas, se pone el botón para devolverse a la página principal, se hacen cambios en la parte visual.

--- source/JunquillalUserSystem/JunquillalUserSystem/Controllers/HomeController.cs
+++ source/JunquillalUserSystem/JunquillalUserSystem/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace JunquillalUserSystem.Controllers
             return View();
         }
 
+        public IActionResult FormularioCantidadPersonas()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
