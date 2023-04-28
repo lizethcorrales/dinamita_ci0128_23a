964100eb26dee53ee1a36f6b8b2159a493caa757
EstebanMora16 <esteban.moragarcia@ucr.ac.cr>
2023-04-28 12:11:27 -0600

    Se crea ruta a archivo JavaScript donde se encuentran validaciones de los datos, solo se tiene validación nombre

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/wwwroot/js/ReservacionesScript.js
@@ -0,0 +1,14 @@
+﻿// location.href='../Home/DatosReserva'"
+const divNombre = document.querySelector("#divNombre");
+var nombre = document.getElementById("nombre");
+console.log(divNombre.lastElementChild);
+
+
+continuar.addEventListener("click", (e) => {
+	if (nombre.value.length < 1) {
+
+		divNombre.lastElementChild.textContent = "Nombre NO válido";
+		console.log(divNombre.lastElementChild);
+	}
+
+});
