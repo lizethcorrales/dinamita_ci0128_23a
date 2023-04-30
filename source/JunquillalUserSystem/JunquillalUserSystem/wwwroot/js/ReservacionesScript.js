e35af09aca07bad0d0a61d26cb99f4a41e000635
lizethcorrales <69595734+lizethcorrales@users.noreply.github.com>
2023-04-30 14:43:38 -0600

    Se añadió modificó validación de campos y se añadió el cambió de página

--- source/JunquillalUserSystem/JunquillalUserSystem/wwwroot/js/ReservacionesScript.js
+++ source/JunquillalUserSystem/JunquillalUserSystem/wwwroot/js/ReservacionesScript.js
@@ -15,18 +15,32 @@ var identificacion = document.getElementById("identificacion");
 
 
 continuar.addEventListener("click", (e) => {
+	var pasar = true;
 	if (nombre.value.length < 1) {
 		divNombre.lastElementChild.textContent = "Nombre NO válido";
+		pasar = false;
+	} else {
+		divNombre.lastElementChild.textContent = "";
 	}
-	console.log(primerApellido.value);
-	if (primerApellido.value.length < 1) {	
+	if (primerApellido.value.length < 1) {
 		divApellido1.lastElementChild.textContent = "Apellido NO válido";
+		pasar = false;
+	} else {
+		divApellido1.lastElementChild.textContent = "";
 	}
 	if (segundoApellido.value.length < 1) {
 		divApellido2.lastElementChild.textContent = "Apellido NO válido";
+		pasar = false;
+	} else {
+		divApellido2.lastElementChild.textContent = "";
 	}
 	if (identificacion.value.length < 1) {
 		divIdentificacion.lastElementChild.textContent = "Identificación NO válido";
+		pasar = false;
+	} else {
+		divIdentificacion.lastElementChild.textContent = "";
 	}
-
+	if (pasar) {
+		location.href = '../Home/DatosReserva';
+	}
 });
