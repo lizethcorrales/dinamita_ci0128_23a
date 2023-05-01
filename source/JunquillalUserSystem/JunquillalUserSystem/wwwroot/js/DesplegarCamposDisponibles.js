6eae62a3dfc47276c1220eba68c5d56319ae904e
EstebanMora16 <esteban.moragarcia@ucr.ac.cr>
2023-05-01 12:03:33 -0600

    Se crea funcionalidad para comprobar los campos disponibles en una fecha específica

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/wwwroot/js/DesplegarCamposDisponibles.js
@@ -0,0 +1,12 @@
+﻿var fechaEntrada = document.getElementById("fecha-entrada");
+var casetilla = document.getElementById("casetilla");
+var online = document.getElementById("online");
+
+buscar.addEventListener("click", (e) => {
+    console.log("Entra");
+    console.log(fechaEntrada.value);
+    if (fechaEntrada.value.length != 0) {
+        //casetilla.setAttribute("value", "30");
+        casetilla.textContent = "30";
+    }
+});
