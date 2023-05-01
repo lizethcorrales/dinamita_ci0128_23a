f29d559551016ff5fd44e00c6fe473e6cdcf8703
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-04-30 20:21:52 -0600

    Vistas del calendario,formulario y datos del huesped conectadas dentro del ReservacionController

--- source/JunquillalUserSystem/JunquillalUserSystem/Controllers/ReservacionController.cs
+++ source/JunquillalUserSystem/JunquillalUserSystem/Controllers/ReservacionController.cs
@@ -1,20 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace JunquillalUserSystem.Controllers
 {
     public class ReservacionController : Controller
     {
-        public IActionResult Calendario()
-        {
-            var reservedDates = new[] {"2023-05-02", "2023-05-03", "2023-05-04" };
-            ViewBag.reservedDates = reservedDates;
-            return View();
-            
-        }
-
         public IActionResult FormularioCantidadPersonas()
         {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calendario()
+        {
+          
+
+            // Imprimir los valores recibidos del formulario
+            System.Diagnostics.Debug.Write($"Cantidad adultos nacionales: {Request.Form["cantidad_Adultos_Nacional"]}");
+            var reservedDates = new[] { "2023-05-02", "2023-05-03", "2023-05-04", "2023-05-30" };
+            ViewBag.reservedDates = reservedDates;
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Reservaciones(string startDate, string endDate)
+        {
+            var fechaEntrada = Request.Form["fecha-entrada"];
+            var fechaSalida = Request.Form["fecha-salida"];
+            System.Diagnostics.Debug.Write($"Fecha entrada: {fechaEntrada}");
+            System.Diagnostics.Debug.Write($"Fecha salida: {fechaSalida}");
+
+
             return View();
         }
     }
