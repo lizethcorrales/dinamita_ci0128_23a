9d37fc8a900937693c3973cf177bc67e2ded0985
EstebanMora16 <esteban.moragarcia@ucr.ac.cr>
2023-05-03 10:56:46 -0600

    Se crea un controlador para los campos disponibles, se crea un directorio para la vista de campos disponibles y se actualiza el layout

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/Controllers/CamposDisponiblesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JunquillalUserSystem.Controllers
+{
+    public class CamposDisponiblesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CamposDisponibles()
+        {
+            return View();
+        }
+    }
+}
--- source/JunquillalUserSystem/JunquillalUserSystem/Controllers/ReservacionController.cs
+++ source/JunquillalUserSystem/JunquillalUserSystem/Controllers/ReservacionController.cs
@@ -17,5 +17,6 @@ namespace JunquillalUserSystem.Controllers
         {
             return View();
         }
+        
     }
 }
