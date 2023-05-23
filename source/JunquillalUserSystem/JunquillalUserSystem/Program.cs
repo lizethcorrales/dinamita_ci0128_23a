048b07c9931aee3c3e7fec2fc724c7c75961a0c0
SabrinaBH <113212565+SabrinaBH@users.noreply.github.com>
2023-05-23 14:09:01 -0600

    Revert "Estilo campos disponibles"

--- source/JunquillalUserSystem/JunquillalUserSystem/Program.cs
+++ source/JunquillalUserSystem/JunquillalUserSystem/Program.cs
@@ -20,18 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
-
-    endpoints.MapControllerRoute(
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-});
-
-
 
 app.Run();
