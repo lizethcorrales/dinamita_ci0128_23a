90939e3f2572d29b30438c62bbe6615cf7c41a9c
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-05-24 23:49:15 -0600

    Se crea el controlador de administrar reservas y su respectiva vista, aun no muestra las reservass ni tampoco las elimina

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/Areas/Admin/Controllers/AdministrarReservasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JunquillalUserSystem.Models;
+
+namespace JunquillalUserSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdministrarReservasController : Controller
+    { 
+        
+        public IActionResult Reservas()
+        {
+            List<ReservacionModelo> listaReservas = new List<ReservacionModelo>();
+            return View(listaReservas);
+        }
+    }
+}
