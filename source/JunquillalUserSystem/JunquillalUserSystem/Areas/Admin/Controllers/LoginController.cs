1e5612e8772693a687ad5579da02195e6f35bdab
GabrielChaconGarro <gabriel.chacon@ucr.ac.cr>
2023-05-29 22:53:19 -0600

    Se añaden el modelo y las vistas que serán utilizados para hacer el login

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JunquillalUserSystem.Models;
+using JunquillalUserSystem.Areas.Admin.Controllers.Handlers;
+
+namespace JunquillalUserSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Bienvenida()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/JunquillalUserSystem/JunquillalUserSystem/Models/TrabajadorModelo.cs
@@ -0,0 +1,33 @@
+﻿namespace JunquillalUserSystem.Models
+{
+    public class TrabajadorModelo
+    {
+        public string ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string correo { get; set; }
+        public string Puesto { get; set; }
+
+        public TrabajadorModelo() { 
+            ID = 0;
+            Nombre = "";
+            Apellido1 = "";
+            Apellido2 = "";
+            correo = "";
+            Puesto = "";
+
+        }
+
+        public TrabajadorModelo obtenerCredenciales(IFormCollection form)
+        {
+            TrabajadorModelo trabajador = new TrabajadorModelo();
+            trabajador.ID  = form["usuario"];
+
+
+            return trabajador;
+
+        }
+        
+    }
+}
