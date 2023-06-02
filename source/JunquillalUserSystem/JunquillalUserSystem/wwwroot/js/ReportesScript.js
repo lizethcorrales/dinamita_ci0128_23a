11e07925db7601720db049a00dd49390d9c56f25
EstebanMora16 <esteban.moragarcia@ucr.ac.cr>
2023-06-01 19:48:32 -0600

    Cuando el reporte es diario solo muestra un calendario, cuando el reporte es de varios días se muestran dos calendarios para tener el rando de fechas

---
+++ source/JunquillalUserSystem/JunquillalUserSystem/wwwroot/js/ReportesScript.js
@@ -0,0 +1,16 @@
+﻿var divCalendarioRangoDias = document.getElementById("calendarioRangoDias");
+
+var calendarioRangoDias = document.getElementById("visitas");
+var calendarioDiario = document.getElementById("diario");
+
+calendarioRangoDias.addEventListener("change", (e) => {
+    if (calendarioRangoDias.checked) {
+        divCalendarioRangoDias.hidden = false;
+    }     
+});
+
+calendarioDiario.addEventListener("change", (e) => {
+    if (calendarioDiario.checked) {
+        divCalendarioRangoDias.hidden = true;
+    }
+});
