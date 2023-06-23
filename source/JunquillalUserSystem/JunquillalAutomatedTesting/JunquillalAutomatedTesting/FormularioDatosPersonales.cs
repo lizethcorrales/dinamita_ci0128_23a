2befd2d45017961389379693c089d076b8401e9e
Lizeth Corrales Cortés <69595734+lizethcorrales@users.noreply.github.com>
2023-06-22 21:57:42 -0600

    Se crean las primeras tres pruebas automatizadas

---
+++ source/JunquillalUserSystem/JunquillalAutomatedTesting/JunquillalAutomatedTesting/FormularioDatosPersonales.cs
@@ -0,0 +1,108 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace JunquillalAutomatedTesting
+{
+    [TestFixture]
+    public class Tests
+    {
+        IWebDriver driver = null;
+        public Tests()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7042/Reservacion/FormularioCantidadPersonas");
+            driver.Manage().Window.Maximize();
+        }
+
+        /*[TearDown]
+        public void tearDown()
+        {
+            driver.Quit();
+        }*/
+
+        [Test, Order(1)]
+        public void ComprobarValidacionCampos()
+        {    
+            irAPaginaFormularioDatosPersonales();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement botonContinuar = wait.Until(e => e.FindElement(By.Id("continuar")));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].click();", botonContinuar);
+            IWebElement mensajeErrorNombre = driver.FindElement(By.Id("mensajeErrorNombre"));
+            IWebElement mensajeErrorApellido = driver.FindElement(By.Id("mensajeErrorApellido1"));
+            IWebElement mensajeErrorIdentificacion = driver.FindElement(By.Id("mensajeErrorIdent"));
+            Assert.AreEqual("Nombre NO válido", mensajeErrorNombre.Text);
+            Assert.AreEqual("Apellido NO válido", mensajeErrorApellido.Text);
+            Assert.AreEqual("Identificación NO válido", mensajeErrorIdentificacion.Text);
+        }
+
+        [Test, Order(2)]
+        public void ComprobarQueExisteProvinciaConIdentificacionNacional()
+        {
+            Setup();
+            driver.Navigate().GoToUrl("https://localhost:7042/Reservacion/FormularioCantidadPersonas");
+            irAPaginaFormularioDatosPersonales();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement radioNacional = wait.Until(e => e.FindElement(By.Id("Nacional")));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].click();", radioNacional);
+            var dropDownProvincia = driver.FindElements(By.Id("provincia"));
+            Assert.True(dropDownProvincia.Count == 1);
+        }
+
+        [Test, Order(3)]
+        public void ComprobarQueDesapareceProvinciaConIdentificacionExtranjera()
+        {
+            Setup();
+            driver.Navigate().GoToUrl("https://localhost:7042/Reservacion/FormularioCantidadPersonas");
+            irAPaginaFormularioDatosPersonales();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement radioExtranjero = wait.Until(e => e.FindElement(By.Id("Extranjero")));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].click();", radioExtranjero);
+            var dropDownProvincia = driver.FindElements(By.Id("provincia"));
+            Assert.True(dropDownProvincia.Count == 0);
+        }
+
+        public void CompletarPaginaCantidadPersonas()
+        {
+            IWebElement inputCantidadAdultosNacional = driver.FindElement(By.Name("cantidad_Adultos_Nacional"));
+            IWebElement botonSiguiente = driver.FindElement(By.Id("siguiente_calendario"));
+            inputCantidadAdultosNacional.Clear();
+            inputCantidadAdultosNacional.SendKeys("1"); // Cambia valor del input
+            botonSiguiente.Click();
+        }
+
+        public void CompletarPaginaCalendario()
+        {
+            IWebElement inputFechaEntrada = driver.FindElement(By.Name("fecha-entrada"));
+            IWebElement inputFechaSalida = driver.FindElement(By.Name("fecha-salida"));
+            IWebElement botonSiguiente = driver.FindElement(By.Id("btn-siguiente"));
+            inputFechaEntrada.Click();
+            IWebElement flechaSiguienteMes = driver.FindElement(By.CssSelector(".ui-icon-circle-triangle-e"));
+            flechaSiguienteMes.Click();
+            IWebElement fechaEntradaElegida = driver.FindElement(By.LinkText("19"));
+            fechaEntradaElegida.Click();
+            inputFechaEntrada.Click();
+            inputFechaSalida.Click();
+            IWebElement fechaSalidaElegida = driver.FindElement(By.LinkText("24"));
+            fechaSalidaElegida.Click();
+            botonSiguiente.Click();
+        }
+
+        public void irAPaginaFormularioDatosPersonales()
+        {
+            CompletarPaginaCantidadPersonas();
+            CompletarPaginaCalendario();
+        }
+    }
+}
